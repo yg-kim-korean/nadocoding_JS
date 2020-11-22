1f0f3a74287b89a82bd874a4584325d11677d37e
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-11-23 00:00:15 +0900

    class extends

---
+++ js-object_oriented_programming/class.js
@@ -0,0 +1,20 @@
+class Person{
+    constructor(name, first, second){
+        this.name=name;
+        this.first = first;
+        this.second = second;
+    }
+    sum(){
+        return 'prototype: ' + (this.first+this.second);
+    }
+}
+class PersonPlus extends Person{
+    
+    avg(){
+        return (this.first+this.second)/2;
+    }
+}
+
+var kim = new PersonPlus('kim',10,20);
+console.log(kim.sum());
+console.log(kim.avg());
